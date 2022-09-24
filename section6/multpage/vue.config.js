f77bae6ed48ba6db2f1233ba6c37c12ee7581c7c
m-sato <ssmmaagg36@gmail.com>
2022-09-24 20:11:36 +0900

    セクション6 86まで

---
+++ section6/multpage/vue.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  publicPath: "",
+  pages: {
+    index: {
+      entry: "src/index/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "Index Page",
+      chunks: ["chunk-vendors", "chunk-common", "index"],
+    },
+    users: {
+      entry: "src/users/main.js",
+      template: "public/users.html",
+      filename: "users.html",
+      title: "Users Page",
+      chunks: ["chunk-vendors", "chunk-common", "users"],
+    },
+  },
+});
